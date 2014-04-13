76305f5ce6818011cd26f0d04edf8e2435556642
Pär Bohrarper <par.bohrarper@gmail.com>
2014-04-13 16:14:01 +0200

    Owned boxes not needed for the token lists

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
 mod parser;
 
 fn main() {
-//    println!("ast: {:?}", parser::parse(parser::tokenizer::tokenize("{1 [2 3]}")));
+    println!("ast: {:?}", parser::parse(parser::tokenizer::tokenize("{1 [2 3]}")));
 }
--- parser.rs
+++ parser.rs
@@ -2,39 +2,39 @@ pub mod tokenizer;
 
 #[deriving(Eq, Show, Clone)]
 pub enum AstNode {
-    Leaf(~tokenizer::Token),
+    Leaf(tokenizer::Token),
     Array(~[AstNode]),
     Function(~[AstNode])
 }
 
 // This doesn't look like idiomatic rust..
-fn do_parse(tokens: &[~tokenizer::Token], offset: uint) -> (uint, ~[AstNode]) {
+fn do_parse(tokens: &[tokenizer::Token], offset: uint) -> (uint, ~[AstNode]) {
     let mut ast: ~[AstNode] = ~[];
     let mut i = offset;
     while i < tokens.len() {
         match &tokens[i] {
-            &~tokenizer::EndFunction => {
+            &tokenizer::EndFunction => {
                 return (i + 1, ast);
             },
-            &~tokenizer::EndArray => {
+            &tokenizer::EndArray => {
                 return (i + 1, ast);
             },
-            &~tokenizer::BeginFunction => {
+            &tokenizer::BeginFunction => {
                 let (new_i, tmp) = do_parse(tokens, i + 1);
                 i = new_i;
                 // Check that there was a matching end
                 match &tokens[i-1] {
-                    &~tokenizer::EndFunction => {},
+                    &tokenizer::EndFunction => {},
                     _ => fail!("syntax error")
                 }
                 ast.push(Function(tmp));
             },
-            &~tokenizer::BeginArray => {
+            &tokenizer::BeginArray => {
                 let (new_i, tmp) = do_parse(tokens, i + 1);
                 i = new_i;
                 // Check that there was a matching end
                 match &tokens[i-1] {
-                    &~tokenizer::EndArray => {},
+                    &tokenizer::EndArray => {},
                     _ => fail!("syntax error")
                 }
                 ast.push(Array(tmp));
@@ -48,7 +48,7 @@ fn do_parse(tokens: &[~tokenizer::Token], offset: uint) -> (uint, ~[AstNode]) {
     (i, ast)
 }
 
-pub fn parse(a: ~[~tokenizer::Token]) -> ~[AstNode] {
+pub fn parse(a: ~[tokenizer::Token]) -> ~[AstNode] {
     println!("tokens: {:?}", a);
     let (i, ast) = do_parse(a, 0);
     if i != a.len() {
@@ -61,41 +61,41 @@ pub fn parse(a: ~[~tokenizer::Token]) -> ~[AstNode] {
 #[test]
 #[should_fail]
 fn test_syntax_error() {
-    parse(~[~tokenizer::BeginFunction]);
-    parse(~[~tokenizer::BeginArray]);
-    parse(~[~tokenizer::EndFunction]);
-    parse(~[~tokenizer::EndArray]);
-    parse(~[~tokenizer::BeginFunction,
-            ~tokenizer::Integer(1),
-            ~tokenizer::BeginArray,
-            ~tokenizer::Integer(2),
-            ~tokenizer::Integer(3),
-            ~tokenizer::EndFunction,
-            ~tokenizer::EndArray]);
+    parse(~[tokenizer::BeginFunction]);
+    parse(~[tokenizer::BeginArray]);
+    parse(~[tokenizer::EndFunction]);
+    parse(~[tokenizer::EndArray]);
+    parse(~[tokenizer::BeginFunction,
+            tokenizer::Integer(1),
+            tokenizer::BeginArray,
+            tokenizer::Integer(2),
+            tokenizer::Integer(3),
+            tokenizer::EndFunction,
+            tokenizer::EndArray]);
 }
 
 #[test]
 fn test_parser() {
-    assert_eq!(parse(~[~tokenizer::BeginArray,
-                       ~tokenizer::Integer(1),
-                       ~tokenizer::Integer(2),
-                       ~tokenizer::EndArray]),
-               ~[Array(~[Leaf(~tokenizer::Integer(1)),
-                         Leaf(~tokenizer::Integer(2))])]);
-    assert_eq!(parse(~[~tokenizer::BeginFunction,
-                       ~tokenizer::Integer(1),
-                       ~tokenizer::Integer(2),
-                       ~tokenizer::EndFunction]),
-               ~[Function(~[Leaf(~tokenizer::Integer(1)),
-                            Leaf(~tokenizer::Integer(2))])]);
-    assert_eq!(parse(~[~tokenizer::BeginFunction,
-                       ~tokenizer::Integer(1),
-                       ~tokenizer::BeginArray,
-                       ~tokenizer::Integer(2),
-                       ~tokenizer::Integer(3),
-                       ~tokenizer::EndArray,
-                       ~tokenizer::EndFunction]),
-               ~[Function(~[Leaf(~tokenizer::Integer(1)),
-                            Array(~[Leaf(~tokenizer::Integer(2)),
-                                    Leaf(~tokenizer::Integer(3))])])])
+    assert_eq!(parse(~[tokenizer::BeginArray,
+                       tokenizer::Integer(1),
+                       tokenizer::Integer(2),
+                       tokenizer::EndArray]),
+               ~[Array(~[Leaf(tokenizer::Integer(1)),
+                         Leaf(tokenizer::Integer(2))])]);
+    assert_eq!(parse(~[tokenizer::BeginFunction,
+                       tokenizer::Integer(1),
+                       tokenizer::Integer(2),
+                       tokenizer::EndFunction]),
+               ~[Function(~[Leaf(tokenizer::Integer(1)),
+                            Leaf(tokenizer::Integer(2))])]);
+    assert_eq!(parse(~[tokenizer::BeginFunction,
+                       tokenizer::Integer(1),
+                       tokenizer::BeginArray,
+                       tokenizer::Integer(2),
+                       tokenizer::Integer(3),
+                       tokenizer::EndArray,
+                       tokenizer::EndFunction]),
+               ~[Function(~[Leaf(tokenizer::Integer(1)),
+                            Array(~[Leaf(tokenizer::Integer(2)),
+                                    Leaf(tokenizer::Integer(3))])])])
 }

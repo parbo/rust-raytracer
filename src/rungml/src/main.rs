197466e6ff4611947382a53e30cd63b4e279b63f
Pär Bohrarper <par.bohrarper@gmail.com>
2018-09-23 19:11:52 +0200

    make binary separate

--- src/rungml/src/main.rs
+++ src/rungml/src/main.rs
@@ -1,11 +1,18 @@
 extern crate rust_raytracer;
+extern crate getopts;
 
+use getopts::Options;
 use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::path::Path;
 use std::collections::HashSet;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
 
 fn get_contents(path: &Path, defines: &mut HashSet<String>) -> Result<Vec<String>, Box<std::error::Error>> {
     let input = File::open(path)?;
@@ -38,11 +45,37 @@ fn get_contents(path: &Path, defines: &mut HashSet<String>) -> Result<Vec<String
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 
-    let filename = &args[1];
+    let mut opts = Options::new();
+    opts.optopt("o", "", "set output file name", "NAME");
+    opts.optflag("n", "no-render", "don't run the gml");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+
     let mut defines = HashSet::<String>::new();
-    if let Ok(lines) = get_contents(Path::new(filename), &mut defines) {
+    if let Ok(lines) = get_contents(Path::new(&input), &mut defines) {
         let contents = lines.join("\n");
-        rust_raytracer::render::render_gml(&contents);
+        if let Some(output) = matches.opt_str("o") {
+            println!("out file: {}", output);
+            std::fs::write(output, contents.clone()).expect("can't write file");
+        }
+        if !matches.opt_present("n") {
+            rust_raytracer::render::render_gml(&contents);
+        }
     }
 }
